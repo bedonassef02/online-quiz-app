5725b987773819113a4e5a9966fe087e61b4cad2
Bedo <bedonassef71@gmail.com>
2024-04-05 13:05:24 +0200

    format

--- src/grade/model/grade.model.js
+++ src/grade/model/grade.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const gradeSchema = new mongoose.Schema(
   {
     userId: {
-      type:mongoose.Schema.Types.ObjectId,
+      type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
       required: true,
     },
--- src/question/utils/middlewares/is-auth-to-delete.js
+++ src/question/utils/middlewares/is-auth-to-delete.js
@@ -2,7 +2,7 @@ const questionService = require('../../question.service');
 const quizService = require('../../../quiz/quiz.service');
 
 const isAuthToDelete = async (req, res, next) => {
- try {
+  try {
     const userId = req.user.id;
     const { id } = req.params;
 
@@ -15,18 +15,22 @@ const isAuthToDelete = async (req, res, next) => {
     const quiz = await quizService.findOne(question.quizId);
 
     if (!quiz || quiz.userId.toJSON() !== userId) {
-      return res.status(401).json({ message: 'You are not the owner of this quiz' });
+      return res
+        .status(401)
+        .json({ message: 'You are not the owner of this quiz' });
     }
 
     next();
- } catch (error) {
+  } catch (error) {
     // Log the error for debugging purposes
     console.error(error);
     // Send a generic error response
-    res.status(500).json({ message: 'An error occurred while processing your request' });
- }
+    res
+      .status(500)
+      .json({ message: 'An error occurred while processing your request' });
+  }
 };
 
 module.exports = {
- isAuthToDelete,
+  isAuthToDelete,
 };
