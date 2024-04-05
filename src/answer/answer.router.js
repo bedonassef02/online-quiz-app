ba576578e5023c582c1f6af0f4a1ffca533b0491
Bedo <bedonassef71@gmail.com>
2024-04-05 04:12:43 +0200

    remove unused packages

--- src/answer/answer.router.js
+++ src/answer/answer.router.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const answerController = require('./answer.controller');
 const handleAnswerValidator = require('./utils/validators/handle-answer-creation');
 
-router.post('/',handleAnswerValidator, answerController.create);
+router.post('/', handleAnswerValidator, answerController.create);
 
 module.exports = router;
